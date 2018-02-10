88b6494c0ff77fe1acdc189df53fdf34e1cbc852
Sreejith S <sreejith2904@gmail.com>
2018-02-10 13:02:57 -0500

    testing gym enviroments

---
+++ environments-test.py
@@ -0,0 +1,33 @@
+import gym
+
+__author__ = "Sreejith Sreekumar"
+__email__ = "sreekumar.s@husky.neu.edu"
+__version__ = "0.0.1"
+
+## Loading the cartpole, space invaders and mspacman environments from gym
+
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) 
+
+
+
+env = gym.make('SpaceInvaders-v0')
+env.reset()
+env.render()    
+
+
+
+env = gym.make('MsPacman-v0')
+for i_episode in range(20000):
+    observation = env.reset()
+    for t in range(10000):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
