ff76d156c51e55009bca3bc93421527c7582564d
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-02-14 19:11:09 -0500

    basic policy for cartpole

---
+++ policy.py
@@ -0,0 +1,23 @@
+
+import gym
+env = gym.make("CartPole-v0")
+
+def basic_policy(obs):
+    angle = obs[2]
+    return 0 if angle < 0 else 1
+
+
+totals = []
+
+for episode in range(500):
+    episode_rewards = 0
+    obs = env.reset()
+
+    for step in range(1000):
+        action = basic_policy(obs)
+        obs, reward, done, info = env.step(action)
+        episode_rewards += reward
+    
+    if done:
+        break
+    totals.append(episode_rewards)
