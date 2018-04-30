90061a4eedb2390518106ffc15a5309f94df29d4
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-04-30 15:53:30 -0400

    Fix issues with environment. Add proper control.

--- easy21/environment.py
+++ easy21/environment.py
@@ -10,54 +10,54 @@ __version__ = "0.0.1"
 #                     2) A deck to pick a card from
 #
 
+
 class GameState:
 
-    def __init__(self, player_sum, dealer_sum, is_terminal):
+    def __init__(self, player_sum, dealer, is_terminal):
         self._player_sum = player_sum
-        self._dealer_sum = dealer_sum
+        self._dealer = dealer
         self._is_terminal = is_terminal
 
-        
+    def __str__(self):
+        return "{ Player Sum: " + str(self._player_sum) + "," +\
+            " Dealer Sum: " + str(self._dealer._total) + "," +\
+            " Is Terminal: " + str(self._is_terminal) + "," +\
+            "}"
+
 
 class Environment:
 
-    
     def __init__(self):
+        '''
+        Do not touch the deck and dealer directly
+        '''
         self._deck = Deck()
         self._dealer = Dealer()
+        
         initial_player_sum = 0
         self._game_state = GameState(initial_player_sum,
-                                    self._dealer._total,
-                                    False)
-        
+                                     self._dealer,
+                                     False)
         self.collect_initial_cards()
 
         self.agent_max_value = 21
         self.dealer_max_value = 10
         self.actions_count = 2
-        
-        
 
     def collect_initial_cards(self):
         card_for_dealer = self._deck.pick_starting_black_card()
         card_for_player = self._deck.pick_starting_black_card()
 
-        self._dealer._total += card_for_dealer._value
-        self._game_state._player_sum += card_for_player._value
-        
+        self._game_state._dealer._total = card_for_dealer._value
+        self._game_state._player_sum = card_for_player._value
 
-        
-    
-    def game_state(self):
-        return self.game_state
-
-    
     def check_bust(self, is_agent=False):
         if is_agent:
-            return self._game_state._player_sum > 21 or self._game_state._player_sum <= 1
-        
-        return self._dealer._total > 21 or self._dealer._total <= 1
-        
+            return self._game_state._player_sum > 21 or \
+                self._game_state._player_sum <= 1
+
+        return self._game_state._dealer._total > 21 or \
+            self._game_state._dealer._total <= 1
 
     def _add_card_value(self, card, is_agent=False):
         value = 0
@@ -69,21 +69,18 @@ class Environment:
         if is_agent:
             self._game_state._player_sum += value
         else:
-            self._dealer._total += value
-        
-            
+            self._game_state._dealer._total += value
+
     def _make_dealer_moves(self):
-        
         action = None
-        dealer_sum = 0
-        
+
         while not self._game_state._is_terminal and action != Action.STICK:
-            action = self._dealer._policy()
+            action = self._game_state._dealer._policy()
             if action == Action.HIT:
                 card = self._deck.pick_card()
+                print("Dealer picked: ", card)
                 self._add_card_value(card)
             self._game_state._is_terminal = self.check_bust()
-
 
     def _check_higher_total_and_give_reward(self):
         if self._game_state._player_sum > self._dealer._total:
@@ -92,15 +89,13 @@ class Environment:
             return 0
         else:
             return -1
-        
-        
+
     def step(self, player_action):
 
         reward = 0
-
         if player_action == Action.STICK:
             # Player stopped playing. Now the dealer makes moves
-            next_state = self._make_dealer_moves()
+            self._make_dealer_moves()
             if self._game_state._is_terminal:
                 reward = 1
             else:
@@ -117,7 +112,19 @@ class Environment:
 
         return reward
 
-                
-            
-# Testing the environment
-# env = Environment().step(Action.STICK)
+
+# --------  Testing -------- #
+# (Yes, I know...But I don't have time for a proper test suite)
+
+# Creating the environment
+# env = Environment()
+
+# Checking the dealer moves
+# env._make_dealer_moves()
+
+# player is not playing
+# if the dealer crosses 21 or falls below -1 player wins
+# if the dealer stops between 17 and 21, he wins since the
+# player is not playing and only maintains his initial sum of 0
+
+# reward = env.step(Action.STICK)
